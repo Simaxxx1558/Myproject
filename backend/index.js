b668d256af75cb03ddc6f212f7f48ed035d96dea
Simaxxx1558 <kazimire192@gmail.com>
2025-05-23 21:42:13 +0700

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,16 @@
-onst express = require('express');
+const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin: "https://myproject-xbvq.vercel.app" 
+}));
 
-// Регистрация
-app.post('/api/register', (req, res) => {
-    const { email, password } = req.body;
-    console.log('Получен запрос на регистрацию:', { email, password });
-    res.json({ success: true, message: 'Регистрация успешна!' });
-});
-
-// Вход
-app.post('/api/login', (req, res) => {
-    const { email, password } = req.body;
-    console.log('Получен запрос на вход:', { email, password });
-    res.json({ success: true, token: 'fake-jwt-token' });
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Backend работает!' });
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 Сервер запущен на порту ${PORT}`);
+    console.log(`Server запущен на порту ${PORT}`);
 });
